d30492ff399f28ea1553e65b5810539ba97f1cdc
Erlangga Ibrahim <wowotek@icloud.com>
2021-06-24 06:48:53 +0700

    added ignore build and dist file

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,12 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  verbose: true
+  verbose: true,
+  collectCoverage: true,
+  coverageProvider: "babel",
+  modulePathIgnorePatterns: [
+    "build",
+    "dist",
+    "node_modules"
+  ]
 };
